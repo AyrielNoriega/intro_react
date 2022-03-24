0448bbb31ede8ea45404263be543ceab6941621d
AyrielNoriega <anoriega2310@gmail.com>
2022-03-24 00:10:43 -0500

    creando componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
-import TodoAqui from './TodoCounter';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from "./TodoSearch";
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
 
 const   todos = [
-    { text: 'Cortar cebolla', completed: false},
-    { text: 'Tomar curso', completed: false},
-    { text: 'Llar con la llorona', completed: false}
+    { id: 1, text: 'Cortar cebolla', completed: false},
+    { id: 2, text: 'Tomar curso', completed: false},
+    { id: 3, text: 'Llar con la llorona', completed: false}
 ];
 
 function App() {
   return (
     <React.Fragment>
-        <TodoAqui />
-        {/* <TodoSearch /> */}
-            <input placeholder="Cebolla" />
-        {/* <TodoList> 
+        <TodoCounter />
+        <TodoSearch />
+        <TodoList>
             {todos.map(todo => (
-                <TodoItem />
+                <TodoItem key={todo.id} text={todo.text} />
             ))}
-        </TodoList> */}
+        </TodoList>
 
-        {/* <CreateTodoButton /> */}
-        <button>+</button>
+        <CreateTodoButton />
     </React.Fragment>
   );
 }
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -7,4 +7,4 @@ function TodoCounter() {
     );
 }
 
-export default TodoCounter;
+export {TodoCounter};
