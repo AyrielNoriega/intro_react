abb498e6e602edeb217fb290ecf8dd114ff14a69
AyrielNoriega <anoriega2310@gmail.com>
2022-04-05 15:30:03 -0500

    modificando button

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -43,6 +43,7 @@ function AppUI() {
         )}
         <CreateTodoButton 
             setOpenModal={setOpenModal}
+            openModal={openModal}
         />
         </React.Fragment>
     );
--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -4,7 +4,7 @@ import './CreateTodoButton.css';
 function CreateTodoButton(props) {
 
     const onClickButton = (msg) =>{
-        props.setOpenModal(true);
+        props.setOpenModal(!props.openModal);
     }
 
 
